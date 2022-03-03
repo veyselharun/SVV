090cbf6b365fe479b950b108418a11b995c1dc7a
Veysel Harun ŞAHİN <vsahin@sakarya.edu.tr>
2022-03-03 15:43:02 +0300

    Add JUnitExample1

---
+++ JUnitExample1/src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+
+
+public class Calculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ JUnitExample1/src/test/java/CalculatorTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void add() {
+        assertEquals(10, Calculator.add(3, 7), "Sum is incorrect");
+    }
+}
