4a9aca1d545bec9147ed85e7e25184d2617a772f
Veysel Harun ŞAHİN <vsahin@sakarya.edu.tr>
2022-03-20 11:28:40 +0300

    Add TDDExample

---
+++ TDDExample/src/test/java/CalculatorTest.java
@@ -0,0 +1,54 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testDivision1() {
+        assertEquals(5, Calculator.divide(10, 2));
+    }
+
+    @Test
+    void testDivision2() {
+        assertEquals(2.5, Calculator.divide(10, 4));
+    }
+
+    @Test
+    void testDivision3() {
+        assertEquals(2.5, Calculator.divide(12.5, 5));
+    }
+
+    @Test
+    void testDivision4() {
+        assertEquals(4, Calculator.divide(10, 2.5));
+    }
+
+    @Test
+    void testDivision5() {
+        assertEquals(5, Calculator.divide(12.5, 2.5));
+    }
+
+    @Test
+    void testDivision6() {
+
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> Calculator.divide(12.5, 0),
+                "IllegalArgumentException expected."
+        );
+
+    }
+}
