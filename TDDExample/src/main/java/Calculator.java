94696fe489b32f410d9aaf08553538161a9b5977
Veysel Harun ŞAHİN <vsahin@sakarya.edu.tr>
2022-03-31 08:12:42 +0300

    Update

--- TDDExample/src/main/java/Calculator.java
+++ TDDExample/src/main/java/Calculator.java
@@ -1,30 +1,11 @@
 
 public class Calculator {
 
-    /*
-    public static double divide(int a, int b) {
-        double result;
-        result = (double) a / (double) b;
-        return result;
-    }
-
-    public static double divide(double a, int b) {
-        double result;
-        result = (double) a / (double) b;
-        return result;
-    }
-
-    public static double divide(int a, double b) {
-        double result;
-        result = (double) a / (double) b;
-        return result;
-    }
-     */
 
     public static double divide(double a, double b) {
         double result;
         if (b == 0) throw new IllegalArgumentException("Divide by zero exception.");
-        result = (double) a / (double) b;
+        result = a / b;
         return result;
     }
 
