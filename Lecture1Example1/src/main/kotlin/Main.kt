7a0153d95de299d49662787426d12a3cb6f3881f
Veysel Harun ŞAHİN <vsahin@sakarya.edu.tr>
2023-02-27 14:58:13 +0300

    Fault, Error, Failure Example

--- Lecture1Example1/src/main/kotlin/Main.kt
+++ Lecture1Example1/src/main/kotlin/Main.kt
@@ -5,8 +5,10 @@
 
 fun main(args: Array<String>) {
 
-    // First test case
-    // Fault does not result with failure
+    /*
+     * First test case
+     * Fault does not result with failure
+     */
     val myArray1: IntArray = intArrayOf(5, 2, 3)
 
     print("Values of the first array:")
@@ -19,8 +21,10 @@ fun main(args: Array<String>) {
     println("There are $evenNumbersCount1 even numbers in the array.")
 
 
-    // Second test case
-    // Fault results with failure
+    /*
+     * Second test case
+     * Fault results with failure
+     */
     val myArray2: IntArray = intArrayOf(4, 8, 5)
 
     print("Values of the second array:")
@@ -45,27 +49,30 @@ fun countEvenNumbers(myArray: IntArray): Int {
 
     val myArrayLength: Int = myArray.size - 1
 
-    // For both of the test cases the fault results with an
-    // error, incorrect program state at this point.
-    // Error: i must be 0 not 1 in the first iteration.
-    // Program State of this error:
-    // Program counter (PC) = i < myArrayLength
-    // i = 1 (must be 0 in the first iteration)
-    // count = 0
-
-    // For the first test case the program produces the expected output.
-    // There is no failure.
-    // Error did not result with failure. It did not propagate to the
-    // value of even number count.
-    // count = 1 -> Expected value 1, actual value 1
-
-    // For the second test case the program does produce the expected output.
-    // There is failure.
-    // Error resulted with failure. It propagated to the
-    // value of even number count.
-    // count = 1 -> Expected value 2, actual value 1
-
-    // Fault: i must start from 0 not 1
+    /*
+     * In this example there is a fault.
+     * The for loop must start 0 not 1.
+     *
+     * For both of the test cases the fault results with an
+     * error, incorrect program state at this point.
+     * Error: i must be 0 not 1 in the first iteration.
+     * Program State of this error:
+     * Program counter (PC) = i < myArrayLength
+     * i = 1 (must be 0 in the first iteration)
+     * count = 0
+     *
+     * For the first test case the program produces the expected output.
+     * There is no failure.
+     * Error did not result with failure. It did not propagate to the
+     * value of even number count.
+     * count = 1 -> Expected value 1, actual value 1
+     *
+     * For the second test case the program does produce the expected output.
+     * There is failure.
+     * Error resulted with failure. It propagated to the
+     * value of even number count.
+     * count = 1 -> Expected value 2, actual value 1
+     */
     for (i in 1..myArrayLength) {
         if (myArray[i] % 2 == 0) {
             // The value is even
